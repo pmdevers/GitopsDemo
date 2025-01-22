2e3a57836b9336e726ddf442da641e86b5273906
Patrick Evers <pmdevers@gmail.com>
2025-01-22 14:36:15 +0100

    Update Program.cs

--- src/GitopsDemoApi/Program.cs
+++ src/GitopsDemoApi/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/gitopsdemo", () => "GitOps Demo Response. Updated");
+app.MapGet("/gitopsdemo", () => "GitOps Demo Response. Updated 3");
 
 app.Run();
